4137ecfd854eca0d9f3d815800787aaff801cd02
prathameshgurnule14 <prathameshgurnule14@gmail.com>
2025-02-01 14:42:59 +0530

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Select the elements
+
 let balloon = document.getElementById('balloon');
 let airPumpButton = document.getElementById('airPumpButton');
 let isInflating = false;
@@ -6,29 +6,29 @@ let balloonSize = 100;
 let balloonFlying = false;
 let balloonTimer;
 
-// Array of balloon classes to apply different images
+
 const balloonClasses = ['balloon1', 'balloon2', 'balloon3', 'balloon4', 'balloon5'];
 
-// Function to start the game and reset the balloon
+
 function startNewBalloon() {
     isInflating = false;
     balloonFlying = false;
     balloonSize = 100;
     
-    // Reset balloon properties
+ 
     balloon.style.display = 'block';
     balloon.style.width = `${balloonSize}px`;
     balloon.style.height = `${balloonSize * 1.5}px`;
     balloon.style.bottom = '0px'; // Reset position
     balloon.style.left = `${Math.random() * (window.innerWidth - 100)}px`; // Random position
     
-    // Assign a random balloon image
-    balloon.className = 'balloon'; // Reset class
+   
+    balloon.className = 'balloon'; 
     let randomBalloon = balloonClasses[Math.floor(Math.random() * balloonClasses.length)];
     balloon.classList.add(randomBalloon);
 }
 
-// Function to start inflating when button is pressed
+
 airPumpButton.onmousedown = () => {
     if (isInflating || balloonFlying) return;
     isInflating = true;
@@ -46,13 +46,13 @@ airPumpButton.onmousedown = () => {
     }, 100);
 };
 
-// Stop inflating when button is released
+
 airPumpButton.onmouseup = () => {
     clearInterval(balloonTimer);
     isInflating = false;
 };
 
-// Function to make the balloon rise slowly
+
 function moveBalloonUp() {
     let balloonBottom = 0;
     const riseInterval = setInterval(() => {
@@ -66,7 +66,7 @@ function moveBalloonUp() {
     }, 20);
 }
 
-// Function to burst and restart the game
+
 function burstBalloon() {
     balloon.style.display = 'none';
     setTimeout(() => {
@@ -74,7 +74,7 @@ function burstBalloon() {
     }, 1000);
 }
 
-// Click event to burst the balloon
+
 balloon.onclick = () => {
     if (balloonFlying) {
         balloonFlying = false;
@@ -82,5 +82,5 @@ balloon.onclick = () => {
     }
 };
 
-// Initialize the first balloon
+
 startNewBalloon();
